9da760ddc1757aa8ba066f7a412e967b95db1a7f
Tim <81256343+syss69@users.noreply.github.com>
2024-01-12 12:35:12 +0000

    Merge pull request #27 from emerson-eps/dependabot/npm_and_yarn/browserify-sign-4.2.2 Images added

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,33 @@ function App() {
   return (
     <div className="App">
       <header role="heading" aria-level={1} className="App-header">
-        <p>
-          Emerson React Coding Challenge
+        <p role="title">
+          Elephant game
         </p>
       </header>
+      <div>       
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Ginger" height="auto" width={96} alt="Smile" id='smile' />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Ginger" height="auto" width={96} alt="Smile" id='smile' />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Cleo" height="auto" width={96} alt="Star Eyes" id='stars' />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Cleo" height="auto" width={96} alt="Star Eyes" id='stars' />
+      </div>
       <div>
-        {
-          /*
-            Please add your custom component(s) here
-          */
-        }
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Mittens" height="auto" width={96} alt="Sunglasses" id="sunglasses" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Mittens" height="auto" width={96} alt="Sunglasses" id="sunglasses" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Charlie" height="auto" width={96} alt="Sad Face" id="sadFAce" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Charlie" height="auto" width={96} alt="Sad Face" id="sadFace" />
+      </div>
+      <div>
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Lucy" height="auto" width={96} alt="Eal" id="eal" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Lucy" height="auto" width={96} alt="Eal" id="eal" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Rascal" height="auto" width={96} alt="Wink" id="wink" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Rascal" height="auto" width={96} alt="Wink" id="wink" />
+      </div>
+      <div>
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Snowball" height="auto" width={96} alt="Kiss" id="Kiss" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Snowball" height="auto" width={96} alt="Kiss" id="Kiss" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Jack" height="auto" width={96} alt="Angry" id="Angry" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Jack" height="auto" width={96} alt="Angry" id="Angry" />
       </div>
     </div>
   );
