032e637ae10d66d535772b95919aa897deb78786
Tim <81256343+syss69@users.noreply.github.com>
2024-01-12 15:57:59 +0000

    Images

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,30 @@ function App() {
           Elephant game
         </p>
       </header>
-      <div>       
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Ginger" height="auto" width={96} alt="Smile" id='smile' />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Ginger" height="auto" width={96} alt="Smile" id='smile' />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Cleo" height="auto" width={96} alt="Star Eyes" id='stars' />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Cleo" height="auto" width={96} alt="Star Eyes" id='stars' />
+      <div>
+        <br/>
+        <br/>
       </div>
       <div>
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Mittens" height="auto" width={96} alt="Sunglasses" id="sunglasses" />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Mittens" height="auto" width={96} alt="Sunglasses" id="sunglasses" />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Charlie" height="auto" width={96} alt="Sad Face" id="sadFAce" />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Charlie" height="auto" width={96} alt="Sad Face" id="sadFace" />
-      </div>
-      <div>
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Lucy" height="auto" width={96} alt="Eal" id="eal" />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Lucy" height="auto" width={96} alt="Eal" id="eal" />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Rascal" height="auto" width={96} alt="Wink" id="wink" />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Rascal" height="auto" width={96} alt="Wink" id="wink" />
-      </div>
-      <div>
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Snowball" height="auto" width={96} alt="Kiss" id="Kiss" />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Snowball" height="auto" width={96} alt="Kiss" id="Kiss" />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Jack" height="auto" width={96} alt="Angry" id="Angry" />
-        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Jack" height="auto" width={96} alt="Angry" id="Angry" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Ginger" height="auto" width={150} alt="Smile" id='smile' />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Ginger" height="auto" width={150} alt="Smile" id='smile' />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Cleo" height="auto" width={150} alt="Star Eyes" id='stars' />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Cleo" height="auto" width={150} alt="Star Eyes" id='stars' />
+        <br />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Mittens" height="auto" width={150} alt="Sunglasses" id="sunglasses" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Mittens" height="auto" width={150} alt="Sunglasses" id="sunglasses" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Charlie" height="auto" width={150} alt="Sad Face" id="sadFAce" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Charlie" height="auto" width={150} alt="Sad Face" id="sadFace" />
+        <br />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Lucy" height="auto" width={150} alt="Eal" id="eal" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Lucy" height="auto" width={150} alt="Eal" id="eal" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Rascal" height="auto" width={150} alt="Wink" id="wink" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Rascal" height="auto" width={150} alt="Wink" id="wink" />
+        <br />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Snowball" height="auto" width={150} alt="Kiss" id="Kiss" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Snowball" height="auto" width={150} alt="Kiss" id="Kiss" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Jack" height="auto" width={150} alt="Angry" id="Angry" />
+        <img src="https://api.dicebear.com/7.x/fun-emoji/svg?seed=Jack" height="auto" width={150} alt="Angry" id="Angry" />
       </div>
     </div>
   );
