fa38e880a7eb6ed5c199b58ec1c49952fb395a8a
Tim <81256343+syss69@users.noreply.github.com>
2024-01-15 09:21:31 +0000

    Test count of images

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,14 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('To have a title', () => {
   render(<App />);
   const title = screen.getByRole("title")
   expect(title).toBeInTheDocument(<p role="title">Elephant game</p>);
 });
+
+test('16 images', () => {
+  render(<App />);
+  const imgTags = screen.getAllByRole("img");
+  expect(imgTags).toHaveLength(16);
+})
