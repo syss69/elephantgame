a554906aececb922f058b5d6a3a931b501b8df3c
Tim <81256343+syss69@users.noreply.github.com>
2024-01-16 08:58:54 +0000

    Handle click on card

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,4 +1,5 @@
 import { prependOnceListener } from "process";
+import { useState } from "react";
 
 export interface ICardProps {
     state: boolean;
@@ -8,9 +9,10 @@ export interface ICardProps {
 }
 
 const Card = (props: ICardProps) => {
-    const {state, imageSrc, index, onClick} = props;
-    let src = state === false? "https://api.dicebear.com/7.x/shapes/svg?seed=Harley":imageSrc;
-    return <img src ={src}  height="auto" width={150} alt={`image_${index}`} id={`image_${index}`} onClick={() => onClick(index)}/>;
+    const [status, setStatus] = useState(false)
+    const {imageSrc, index} = props;
+    let src = status === false? "https://api.dicebear.com/7.x/shapes/svg?seed=Harley":imageSrc;
+    return <img src ={src}  height="auto" width={150} alt={'image_${index}'} id={'image_${index}'} onClick={() =>{setStatus((prev) => !prev)}}/>;
 }
 
 export default Card;
