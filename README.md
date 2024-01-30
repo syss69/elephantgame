ab68feea8a36c621c37ee5750d58fca6541a195d
pau <pau@pc-pau-corona8.paradigmgeo.net>
2024-01-30 11:18:45 +0100

    Code improvement

--- README.md
+++ README.md
@@ -1,32 +1,40 @@
 # React Coding Challenge
 
 ## Introduction
+
 This project is a bare-bones React/Typescript template. Candidates who are invited to solve the coding challenge are required to clone this repository and implement any components/hooks/modules/tests they deem necessary for a good design. This challenge is confidential and candidates should keep cloned repositories private. Once implemented, candidates are expected to provide private access to their interviewer(s) for code review.
 
 ## Challenge Description
 
 The challenge consists in implementing a mini memory game in React and Typescript. These are the game rules:
 
--	The landing page should display a grid (4x4) of cards initially laid face down. 
--	Each card hides an image that can be revealed by clicking on the card.
--	The grid has unique pairs of distinct images (8 pairs for a 4x4 grid).
--	Images should display avatars provided by [dicebear http API](https://avatars.dicebear.com/docs/http-api). 
+- The landing page should display a grid (4x4) of cards initially laid face down.
+- Each card hides an image that can be revealed by clicking on the card.
+- The grid has unique pairs of distinct images (8 pairs for a 4x4 grid).
+- Images should display avatars provided by [dicebear http API](https://avatars.dicebear.com/docs/http-api).
 
-    Example:
-    ``` html
-    <img src="https://avatars.dicebear.com/api/bottts/seed-9.svg" height="auto" width={96} alt="Avatar" />
-    ```
--	A countdown timer (30 seconds) is displayed on top of the grid and starts when the first card is revealed.
--	When a user reveals two cards, two outcomes are possible: 
-    -   If both card images match, they remain revealed for the rest of the game.
-    -   Otherwise, these two cards are turned face down again 0.5 second after the second card was revealed.
--	The game ends when the last pair is revealed or when the time runs out.
--	The remaining seconds is considered to be the user score and should be displayed at the end with a “Play again” button.
+  Example:
+
+  ```html
+  <img
+    src="https://avatars.dicebear.com/api/bottts/seed-9.svg"
+    height="auto"
+    width="{96}"
+    alt="Avatar"
+  />
+  ```
+
+- A countdown timer (30 seconds) is displayed on top of the grid and starts when the first card is revealed.
+- When a user reveals two cards, two outcomes are possible:
+  - If both card images match, they remain revealed for the rest of the game.
+  - Otherwise, these two cards are turned face down again 0.5 second after the second card was revealed.
+- The game ends when the last pair is revealed or when the time runs out.
+- The remaining seconds is considered to be the user score and should be displayed at the end with a “Play again” button.
 
 Optional extras:
 
--	Add a "Restart" button for resetting both the grid and the timer at any time during the game.
--	Make the grid size and the timer value configurable from the UI.
+- Add a "Restart" button for resetting both the grid and the timer at any time during the game.
+- Make the grid size and the timer value configurable from the UI.
 
 Candidates should aim to spend no more than 2 hours on this exercise. Please do
 not feel obliged to complete all features or the optional extra requirements. We
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-import Card, {Image} from "./Card";
-import {Button, Box, InputLabel, MenuItem, FormControl, Select, SelectChangeEvent} from '@mui/material/'
-
-
-
-
+import Card, { Image } from "./Card";
+import {
+  Button,
+  Box,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+  SelectChangeEvent
+} from "@mui/material/";
 
 const sourceFront = "https://api.dicebear.com/7.x/fun-emoji/svg?seed=";
 
 const images = [
-  {src:`${sourceFront}Ginger`, id:0},   //same as sourceFront + "Ginger"
-  {src:`${sourceFront}Cleo`, id:1},
-  {src:`${sourceFront}Mittens`, id:2},
-  {src:`${sourceFront}Charlie`, id:3},
-  {src:`${sourceFront}Lucy`, id:4},
-  {src:`${sourceFront}Rascal`, id:5},
-  {src:`${sourceFront}Snowball`, id:6},
-  {src:`${sourceFront}Jack`, id:7},
-]
+  { src: `${sourceFront}Ginger`, id: 0 }, //same as sourceFront + "Ginger"
+  { src: `${sourceFront}Cleo`, id: 1 },
+  { src: `${sourceFront}Mittens`, id: 2 },
+  { src: `${sourceFront}Charlie`, id: 3 },
+  { src: `${sourceFront}Lucy`, id: 4 },
+  { src: `${sourceFront}Rascal`, id: 5 },
+  { src: `${sourceFront}Snowball`, id: 6 },
+  { src: `${sourceFront}Jack`, id: 7 },
+];
 
 interface GameTimer {
   maxTime: number;
@@ -26,31 +30,34 @@ interface GameTimer {
 }
 
 function App() {
-
-  const [cardState, setCardState] = useState([false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]);
+  const [pairs, setPairs] = useState(8);
+  const [cardState, setCardState] = useState(Array(pairs * 2).fill(false));
   const [lastCardIndex, setLastCardIndex] = useState(-1);
   const [cardImages, setCardImages] = useState<Image[]>([
     images[0],
-    images[1],
-    images[2],
-    images[3],
-    images[4],
-    images[5],
-    images[6],
-    images[7],
     images[0],
     images[1],
+    images[1],
+    images[2],
     images[2],
     images[3],
+    images[3],
+    images[4],
     images[4],
     images[5],
+    images[5],
     images[6],
+    images[6],
+    images[7],
     images[7],
   ]);
   const [matchCount, setMatchCount] = useState(0); // used to count matches. so, if matchCount called for 8 times - you win a game
-  const [pairClicks, setPairClicks] = useState(1);  //pairClicks can't be more then 2, to avoid click on 3rd card
-  const [totalClicks, setTotalClicks] = useState(1);  // totalClicks used to count how many clicks is done by the user
-  const [timer, setTimer] = useState<GameTimer>({maxTime: 30, remainingTime: 30});
+  const [pairClicks, setPairClicks] = useState(1); //pairClicks can't be more then 2, to avoid click on 3rd card
+  const [totalClicks, setTotalClicks] = useState(1); // totalClicks used to count how many clicks is done by the user
+  const [timer, setTimer] = useState<GameTimer>({
+    maxTime: 30,
+    remainingTime: 30,
+  });
   const [timerId, setTimerId] = useState<NodeJS.Timeout>();
 
   useEffect(() => {
@@ -59,197 +66,207 @@ function App() {
   }, []); // executes on 'mount'
 
   const buildCard = (index: number, image: Image) => {
-    return <Card image={image} index={index} state={cardState[index]} onClick={onCardClick}/>
-  }
+    return (
+      <Card
+        image={image}
+        index={index}
+        state={cardState[index]}
+        onClick={onCardClick}
+      />
+    );
+  };
 
+  const buildCards = () => {
+    const cards: JSX.Element[] = [];
+    let cardInRow = 0;
+    for (let p = 0; p < pairs * 2; ++p) {
+      cards.push(buildCard(p, cardImages[p]));
+      ++cardInRow;
+      if (cardInRow === 4) {
+        cards.push(<br />);
+        cardInRow = 0;
+      }
+    }
+    return cards;
+  };
 
   const startTimer = () => {
     let tt = timer.maxTime;
-    setTimerId(setInterval(() => {
-      if (tt > 0) {
-        --tt;
-        setTimer({...timer, remainingTime:tt});
-        if (tt <= 0) {
-          loseGame();
+    setTimerId(
+      setInterval(() => {
+        if (tt > 0) {
+          --tt;
+          setTimer({ ...timer, remainingTime: tt });
+          if (tt <= 0) {
+            loseGame();
+          }
         }
-      }
-    }, 1000));
+      }, 1000),
+    );
   };
 
   const stopTimer = () => {
     // stop loop
-    if (timerId) {clearInterval(timerId)};    
+    if (timerId) {
+      clearInterval(timerId);
+    }
     // reset timer
-    setTimer({maxTime: timer.maxTime, remainingTime:timer.maxTime});
+    setTimer({ maxTime: timer.maxTime, remainingTime: timer.maxTime });
     setTimerId(undefined);
-  }
-  
+  };
+
   const shuffleArray = () => {
     let shuffledArray = [...cardImages];
-    let currentIndex = shuffledArray.length,  randomIndex;
-    while (currentIndex > 0) {  
+    let currentIndex = shuffledArray.length,
+      randomIndex;
+    while (currentIndex > 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-  
+
       [shuffledArray[currentIndex], shuffledArray[randomIndex]] = [
         shuffledArray[randomIndex],
         shuffledArray[currentIndex],
-      ]
+      ];
     }
-  
-    setCardImages(shuffledArray);
-  }
 
-  const incrementingPairClick = () => { //PairClicks is used to avoid click on 3rd card
+    setCardImages(shuffledArray);
+  };
+
+  const incrementingPairClick = () => {
+    //PairClicks is used to avoid click on 3rd card
     setPairClicks(pairClicks + 1);
-  }
- 
+  };
+
   const incrementingTotalClick = () => {
     setTotalClicks(totalClicks + 1);
-    console.log('You clicked', totalClicks, 'times') // totalClicks is used to count how many clicks do user for game
-  }
+    console.log("You clicked", totalClicks, "times"); // totalClicks is used to count how many clicks do user for game
+  };
 
   const increaseMatchCount = () => {
-    if (matchCount <= 7) {
+    if (matchCount <= pairs - 1) {
       const newCount = matchCount + 1;
       setMatchCount(newCount);
-      console.log('Match!', newCount);
-      if (newCount === 8) {
+      console.log("Match!", newCount);
+      if (newCount === pairs) {
         winGame();
       }
-    }  
+    }
   };
 
   const newGame = () => {
-    setCardState([false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false]);
+    setCardState(Array(pairs * 2).fill(false));
     setPairClicks(1);
     setTotalClicks(1);
     setMatchCount(0);
     stopTimer();
     shuffleArray();
-  }
-  
-  const flipCard = (index: number, state:boolean) => {
+  };
+
+  const flipCard = (index: number, state: boolean) => {
     setCardState((cardState) => {
       const newState = [...cardState];
       newState[index] = state;
       return newState;
-    })
-  }
+    });
+  };
 
   const winGame = () => {
     stopTimer();
-    const score = timer.remainingTime*100;
-    if(totalClicks <= 34){
-      alert(`You win! Your score is ${score}+ bonus 50 points. Your total score is ${score+50}`);
-    }
-    else{
+    const score = timer.remainingTime * 100;
+    if (totalClicks <= pairs * 4.5) {
+      alert(
+        `You win! Your score is ${score}+ bonus 50 points. Your total score is ${score + 50}`,
+      );
+    } else {
       alert(`You win! Your score is ${score} without any bonus points:(`);
     }
-  }
+  };
 
   const loseGame = () => {
     stopTimer();
     alert("Time's up! Game over.");
     // show answer
-    setCardState([true, true, true, true, true, true, true, true, true, true, true, true, true, true, true, true]); 
-  }
+    setCardState(Array(pairs * 2).fill(true));
+  };
 
   const onCardClick = (index: number) => {
-    if(!timerId){
+    if (!timerId && matchCount !== pairs) {
       startTimer();
     }
-      if (cardState[index] !== true && pairClicks < 3){
-        incrementingTotalClick();
-        incrementingPairClick();
-        const newState = !cardState[index];
-        flipCard(index, newState);
-  
-        if (newState === true && lastCardIndex !== -1 && lastCardIndex !== index) {
-          const sameImages = cardImages[lastCardIndex] === cardImages[index];
-          if(sameImages){            
-            setTimeout(() => {
-              increaseMatchCount();
-              setLastCardIndex(-1);
-              setPairClicks(1);
-            }, 100);
-          }
-          else{
-            setTimeout(() => {
-                flipCard(lastCardIndex, false);
-                flipCard(index, false);     
-              setLastCardIndex(-1);
-              setPairClicks(1);
-            }, 500);
-          }
-              
+    if (cardState[index] !== true && pairClicks < 3) {
+      incrementingTotalClick();
+      incrementingPairClick();
+      const newState = !cardState[index];
+      flipCard(index, newState);
+
+      if (
+        newState === true &&
+        lastCardIndex !== -1 &&
+        lastCardIndex !== index
+      ) {
+        const sameImages = cardImages[lastCardIndex] === cardImages[index];
+        if (sameImages) {
+          setTimeout(() => {
+            increaseMatchCount();
+            setLastCardIndex(-1);
+            setPairClicks(1);
+          }, 100);
+        } else {
+          setTimeout(() => {
+            flipCard(lastCardIndex, false);
+            flipCard(index, false);
+            setLastCardIndex(-1);
+            setPairClicks(1);
+          }, 500);
+        }
       }
-        
-        setLastCardIndex(index);
-      }
+
+      setLastCardIndex(index);
     }
+  };
 
-    const handleChange = (event: SelectChangeEvent) => {
-      const selectedValue = parseInt(event.target.value, 10);
-      setTimer({ maxTime: selectedValue, remainingTime: selectedValue });
-    };
+  const handleChangeTimer = (event: SelectChangeEvent) => {
+    const selectedValue = parseInt(event.target.value, 10);
+    setTimer({ maxTime: selectedValue, remainingTime: selectedValue });
+  };
 
-
-    return (
-      <div className="App">
-        <header role="heading" aria-level={1} className="App-header">
-          <p>
-            Elephant game
-          </p>
-        </header>
-        <div>
-          <br/>
-          <Button type="button" variant="contained" onClick={newGame}>New game</Button> 
-          <br/>
-          <div className="select-div">
-            <Box sx={{maxWidth: 200, marginTop:"1rem"}}>
-              <FormControl fullWidth>
-                <InputLabel id="select-label" >Timer</InputLabel>
-                <Select
-                  labelId="select-label"
-                  id="simple-select"
-                  value={timer.maxTime.toString()}
-                  label="timer"
-                  onChange={handleChange}
-                >
-                  <MenuItem value={30}>30 seconds</MenuItem>
-                  <MenuItem value={40}>40 seconds</MenuItem>
-                  <MenuItem value={50}>50 seconds</MenuItem>
-                </Select>
-              </FormControl>
-            </Box>
-          </div>
-          
-          <p id="timer">{`Only ${timer.remainingTime} seconds left!`}</p>
+  return (
+    <div className="App">
+      <header role="heading" aria-level={1} className="App-header">
+        <p>Elephant game</p>
+      </header>
+      <div>
+        <br />
+        <Button type="button" variant="contained" onClick={newGame}>
+          New game
+        </Button>
+        <br />
+        <div className="select-div">
+          <Box sx={{ maxWidth: 200, marginTop: "1rem" }}>
+            <FormControl fullWidth>
+              <InputLabel id="select-label">Timer</InputLabel>
+              <Select
+                labelId="select-label"
+                id="simple-select"
+                value={timer.maxTime.toString()}
+                label="timer"
+                onChange={handleChangeTimer}
+              >
+                <MenuItem value={30}>30 seconds</MenuItem>
+                <MenuItem value={40}>40 seconds</MenuItem>
+                <MenuItem value={50}>50 seconds</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
         </div>
-        <div className= "App-cardContainer">
-          {buildCard(0, cardImages[0])}
-          {buildCard(1, cardImages[1])}
-          {buildCard(2, cardImages[2])}
-          {buildCard(3, cardImages[3])}
-          <br/>
-          {buildCard(4, cardImages[4])}
-          {buildCard(5, cardImages[5])}
-          {buildCard(6, cardImages[6])}
-          {buildCard(7, cardImages[7])}
-          <br/>
-          {buildCard(8,cardImages[8])}
-          {buildCard(9,cardImages[9])}
-          {buildCard(10,cardImages[10])}
-          {buildCard(11,cardImages[11])}
-          <br/>
-          {buildCard(12,cardImages[12])}
-          {buildCard(13,cardImages[13])}
-          {buildCard(14,cardImages[14])}
-          {buildCard(15,cardImages[15])}
-        </div>
+
+        <p id="timer">{`Only ${timer.remainingTime} seconds left!`}</p>
       </div>
-    );
+      <div className="App-cardContainer">
+        <Box sx={{ width: "100%" }}>{buildCards()}</Box>
+      </div>
+    </div>
+  );
 }
 
 export default App;
